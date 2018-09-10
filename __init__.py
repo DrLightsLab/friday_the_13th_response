fc6a606aa962b65eaab5e8ecb733be9514bcf487
Doctor Light <doctorlightslab@gmail.com>
2018-09-10 19:16:33 -0400

    Adds data directory for json files to be stored and updates __init__ to read from data directory

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def stock():
             foo.close()
         #data = json.dumps({"uom": "YD", "imageUrl": null, "pricingData": [{"site": "4001","city": "INTERNET SITE","phone": "www.joann.com","onHands": "N/A","state": "","desc": ""},{"site": "DC01","city": "HUDSON","phone": "","onHands": "0.0","state": "OH","desc": "HUDSON DISTRIBUTION CENTER"}],"description": "RED/WHITE DOT","upc": "400111616013","thumbUrl": null,"article": "11161601","returnType": "S","message": null,"return": {}})
         #data = json.dumps(data)
-        with open('stock.json', 'rb') as file:
+        with open('./data/stock.json', 'rb') as file:
             data = json.load(file)
             data = json.dumps(data)
     except Exception as e:
