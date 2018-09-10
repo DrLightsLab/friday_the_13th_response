5e7b118ca6b3c8d3ec82821e2aa08521ae1d6c23
Doctor Light <doctorlightslab@gmail.com>
2018-09-10 19:12:15 -0400

    Updated init

--- __init__.py
+++ __init__.py
@@ -13,20 +13,20 @@ def foo():
 @app.route("/stock")
 def stock():
     try:
-        with open('log.txt') as foo:
+        data = {}
+        with open('log.txt', 'w') as foo:
             foo.write('test')
             foo.close()
         #data = json.dumps({"uom": "YD", "imageUrl": null, "pricingData": [{"site": "4001","city": "INTERNET SITE","phone": "www.joann.com","onHands": "N/A","state": "","desc": ""},{"site": "DC01","city": "HUDSON","phone": "","onHands": "0.0","state": "OH","desc": "HUDSON DISTRIBUTION CENTER"}],"description": "RED/WHITE DOT","upc": "400111616013","thumbUrl": null,"article": "11161601","returnType": "S","message": null,"return": {}})
-       #data = json.dumps(data)
-       # with open('stock.json', 'rb') as file:
-           # print(file)
+        #data = json.dumps(data)
+        with open('stock.json', 'rb') as file:
+            data = json.load(file)
+            data = json.dumps(data)
     except Exception as e:
         with open('log.txt', 'w') as log:
-            log.write(e)
+            log.write(str(e))
             log.close()
-        
-#        data = json.load(file)
-#        data = json.dumps(data)
+    # return json.dumps(data)
     return data
 
 if __name__ == "__main__":
