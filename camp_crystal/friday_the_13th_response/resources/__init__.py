22ba2f62d97d388671e4c274aba06501e17412aa
Doctor Light <doctorlightslab@gmail.com>
2019-08-09 15:12:24 -0400

    Adds the inventory api to the application

--- camp_crystal/friday_the_13th_response/resources/__init__.py
+++ camp_crystal/friday_the_13th_response/resources/__init__.py
@@ -3,6 +3,7 @@ from flask_restplus import Api
 from .stock import api as api_stock
 from .article import api as api_article
 from .alert import api as api_alert
+from .inventory import api as api_inventory
 
 blueprint = Blueprint('api', __name__, url_prefix='/jason')
 api = Api(blueprint)
@@ -11,3 +12,4 @@ api = Api(blueprint)
 api.add_namespace(api_stock)
 api.add_namespace(api_article)
 api.add_namespace(api_alert)
+api.add_namcespace(api_inventory)
